791efd27364f66671731bd45b253fbfca0ec655c
vamseeuxd <vamsee.uxd@gmail.com>
2024-11-07 23:31:43 +0530

    aaded firebase

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,14 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideRouter(routes, withPreloading(PreloadAllModules)), provideFirebaseApp(() => initializeApp({"projectId":"anil-orthos","appId":"1:856435712:web:1d15e83d5ab28f479ffce5","storageBucket":"anil-orthos.firebasestorage.app","apiKey":"AIzaSyBrWRFB-9QKb74j-FV182e4Rh4A1JRLOWs","authDomain":"anil-orthos.firebaseapp.com","messagingSenderId":"856435712"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()),
   ],
 });
