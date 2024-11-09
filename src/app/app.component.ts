8e3502000788bd8725302d3d3b7d77fd649b33ec
vamseeuxd <vamsee.uxd@gmail.com>
2024-11-09 17:03:52 +0530

    PWA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import {
   IonApp,
   IonRouterOutlet,
@@ -8,26 +8,48 @@ import {
   IonContent,
   IonMenuButton,
   IonMenu,
-  IonButton, IonItem, IonLabel } from '@ionic/angular/standalone';
+  IonButton,
+  IonItem,
+  IonLabel,
+} from '@ionic/angular/standalone';
 import { DarkModeToggleComponent } from './components/dark-mode-toggle/dark-mode-toggle.component';
 import { LogoutButtonComponent } from './components/logout-button/logout-button.component';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   standalone: true,
-  imports: [IonLabel, IonItem,
-    IonButton,
-    IonContent,
-    IonTitle,
-    IonToolbar,
-    IonHeader,
+  imports: [
+    DarkModeToggleComponent,
+    LogoutButtonComponent,
     IonApp,
     IonMenu,
     IonMenuButton,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
     IonRouterOutlet,
-    DarkModeToggleComponent,
-    LogoutButtonComponent,
+    IonItem,
+    IonMenu,
   ],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  private swUpdate = inject(SwUpdate);
+  ngOnInit() {
+    if (!this.swUpdate.isEnabled) {
+      return;
+    }
+    const updatesAvailable = this.swUpdate.versionUpdates.pipe(
+      filter((evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY')
+    );
+    updatesAvailable.subscribe(() => {
+      this.activateNewVersion();
+    });
+  }
+  activateNewVersion(): void {
+    window.location.reload();
+  }
+}
