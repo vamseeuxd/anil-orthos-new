ad329d85dd8f47fa353b12b95414eab933e968d5
vamseeuxd <vamsee.uxd@gmail.com>
2024-11-10 04:55:30 +0530

    patient-autocomplete-list

--- src/app/components/patient-autocomplete-list/patient-autocomplete-list.component.ts
+++ src/app/components/patient-autocomplete-list/patient-autocomplete-list.component.ts
@@ -1,28 +1,17 @@
-import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
+import { AsyncPipe, NgFor, NgIf } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {
   IonInput,
-  IonContent,
   IonList,
   IonItem,
   IonLabel,
-  IonButton,
   IonCard,
   IonItemSliding,
   IonItemOptions,
   IonItemOption,
-  IonModal,
-  IonHeader,
-  IonToolbar,
-  IonTitle,
-  IonButtons,
-  IonFabButton,
-  IonFab,
-  IonIcon,
 } from '@ionic/angular/standalone';
 import { PatientService } from 'src/app/services/patient/patient.service';
-import { PatientRegistrationFormComponent } from '../patient-registration-form/patient-registration-form.component';
 
 @Component({
   selector: 'app-patient-autocomplete-list',
@@ -30,28 +19,19 @@ import { PatientRegistrationFormComponent } from '../patient-registration-form/p
   templateUrl: './patient-autocomplete-list.component.html',
   styleUrls: ['./patient-autocomplete-list.component.scss'],
   imports: [
-    IonIcon,
-    IonFab,
-    IonFabButton,
-    IonButtons,
-    IonTitle,
-    IonToolbar,
-    IonHeader,
-    IonModal,
     IonItemOption,
     IonItemOptions,
     IonItemSliding,
     IonCard,
-    IonButton,
     IonLabel,
     IonItem,
     IonList,
-    IonContent,
     IonInput,
     FormsModule,
+    AsyncPipe,
+    NgFor,
     IonInput,
-    CommonModule,
-    PatientRegistrationFormComponent,
+    NgIf,
   ],
 })
 export class PatientAutocompleteListComponent {
